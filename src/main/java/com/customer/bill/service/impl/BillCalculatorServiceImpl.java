1c4ac155d2fc0c9b6d82e84937bab98370019042
Raghavender Tangallapally <raghu.t29@gmail.com>
2017-06-19 11:35:47 +0100

    Validations before actual implementation.

--- src/main/java/com/customer/bill/service/impl/BillCalculatorServiceImpl.java
+++ src/main/java/com/customer/bill/service/impl/BillCalculatorServiceImpl.java
@@ -6,6 +6,8 @@ package com.customer.bill.service.impl;
 import java.math.BigDecimal;
 import java.util.List;
 
+import org.apache.commons.lang3.Validate;
+
 import com.customer.bill.model.MenuItem;
 import com.customer.bill.service.BillCalculatorService;
 
@@ -15,19 +17,27 @@ import com.customer.bill.service.BillCalculatorService;
  */
 public class BillCalculatorServiceImpl implements BillCalculatorService {
 
-	/* (non-Javadoc)
-	 * @see com.customer.bill.service.BillCalculatorService#getTotalBillExcludingServiceCharge(java.util.List)
+	/*
+	 * (non-Javadoc)
+	 * 
+	 * @see com.customer.bill.service.BillCalculatorService#
+	 * getTotalBillExcludingServiceCharge(java.util.List)
 	 */
+	@Override
 	public BigDecimal getTotalBillExcludingServiceCharge(List<MenuItem> items) {
-		// TODO Auto-generated method stub
+		Validate.notEmpty(items, "Item collection cannot be null or empty");
 		return null;
 	}
 
-	/* (non-Javadoc)
-	 * @see com.customer.bill.service.BillCalculatorService#getTotalBillIncludingServiceCharge(java.util.List, java.math.BigDecimal)
+	/*
+	 * (non-Javadoc)
+	 * 
+	 * @see com.customer.bill.service.BillCalculatorService#
+	 * getTotalBillIncludingServiceCharge(java.util.List, java.math.BigDecimal)
 	 */
+	@Override
 	public BigDecimal getTotalBillIncludingServiceCharge(List<MenuItem> items, BigDecimal totalPrice) {
-		// TODO Auto-generated method stub
+		Validate.notEmpty(items, "Item collection cannot be null or empty");
 		return null;
 	}
 
--- src/main/java/com/customer/bill/service/impl/ServiceChargeServiceImpl.java
+++ src/main/java/com/customer/bill/service/impl/ServiceChargeServiceImpl.java
@@ -6,6 +6,8 @@ package com.customer.bill.service.impl;
 import java.math.BigDecimal;
 import java.util.List;
 
+import org.apache.commons.lang3.Validate;
+
 import com.customer.bill.model.MenuItem;
 import com.customer.bill.service.ServiceChargeService;
 
@@ -15,11 +17,16 @@ import com.customer.bill.service.ServiceChargeService;
  */
 public class ServiceChargeServiceImpl implements ServiceChargeService {
 
-	/* (non-Javadoc)
-	 * @see com.customer.bill.service.ServiceChargeService#getServiceCharge(java.util.List)
+	/*
+	 * (non-Javadoc)
+	 * 
+	 * @see
+	 * com.customer.bill.service.ServiceChargeService#getServiceCharge(java.util
+	 * .List)
 	 */
+	@Override
 	public BigDecimal getServiceCharge(List<MenuItem> items) {
-		// TODO Auto-generated method stub
+		Validate.notEmpty(items, "Item collection cannot be null or empty");
 		return null;
 	}
 

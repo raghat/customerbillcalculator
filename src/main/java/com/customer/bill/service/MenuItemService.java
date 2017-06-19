1c17946c9fccf6bd09e2a5ef4e6a0a086c4f8ecb
Raghavender Tangallapally <raghu.t29@gmail.com>
2017-06-19 11:09:23 +0100

    Interface correction

--- src/main/java/com/customer/bill/service/MenuItemService.java
+++ src/main/java/com/customer/bill/service/MenuItemService.java
@@ -3,14 +3,18 @@
  */
 package com.customer.bill.service;
 
+import java.util.List;
+
+import com.customer.bill.model.MenuItem;
+
 /**
  * @author raghav
  *
  */
 public interface MenuItemService {
-	public boolean hasAllDrinks();
+	public boolean hasAllDrinks(List<MenuItem> items);
 
-	public boolean hasAtleastOneFoodItem();
+	public boolean hasAtleastOneFoodItem(List<MenuItem> items);
 
-	public boolean hasAtleastOneHotFoodItem();
+	public boolean hasAtleastOneHotFoodItem(List<MenuItem> items);
 }
--- src/main/java/com/customer/bill/service/impl/MenuItemServiceImpl.java
+++ src/main/java/com/customer/bill/service/impl/MenuItemServiceImpl.java
@@ -3,6 +3,9 @@
  */
 package com.customer.bill.service.impl;
 
+import java.util.List;
+
+import com.customer.bill.model.MenuItem;
 import com.customer.bill.service.MenuItemService;
 
 /**
@@ -11,26 +14,32 @@ import com.customer.bill.service.MenuItemService;
  */
 public class MenuItemServiceImpl implements MenuItemService {
 
-	/* (non-Javadoc)
+	/*
+	 * (non-Javadoc)
+	 * 
 	 * @see com.customer.bill.service.MenuItemService#hasAllDrinks()
 	 */
-	public boolean hasAllDrinks() {
+	public boolean hasAllDrinks(List<MenuItem> items) {
 		// TODO Auto-generated method stub
 		return false;
 	}
 
-	/* (non-Javadoc)
+	/*
+	 * (non-Javadoc)
+	 * 
 	 * @see com.customer.bill.service.MenuItemService#hasAtleastOneFoodItem()
 	 */
-	public boolean hasAtleastOneFoodItem() {
+	public boolean hasAtleastOneFoodItem(List<MenuItem> items) {
 		// TODO Auto-generated method stub
 		return false;
 	}
 
-	/* (non-Javadoc)
+	/*
+	 * (non-Javadoc)
+	 * 
 	 * @see com.customer.bill.service.MenuItemService#hasAtleastOneHotFoodItem()
 	 */
-	public boolean hasAtleastOneHotFoodItem() {
+	public boolean hasAtleastOneHotFoodItem(List<MenuItem> items) {
 		// TODO Auto-generated method stub
 		return false;
 	}

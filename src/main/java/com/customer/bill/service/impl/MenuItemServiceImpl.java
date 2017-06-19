ccc08b393bfac35251e5bc0048cd6e7e98917c21
Raghavender Tangallapally <raghu.t29@gmail.com>
2017-06-19 11:18:12 +0100

    Implementation for Item service

--- src/main/java/com/customer/bill/service/impl/MenuItemServiceImpl.java
+++ src/main/java/com/customer/bill/service/impl/MenuItemServiceImpl.java
@@ -5,6 +5,7 @@ package com.customer.bill.service.impl;
 
 import java.util.List;
 
+import com.customer.bill.model.ItemType;
 import com.customer.bill.model.MenuItem;
 import com.customer.bill.service.MenuItemService;
 
@@ -15,33 +16,34 @@ import com.customer.bill.service.MenuItemService;
 public class MenuItemServiceImpl implements MenuItemService {
 
 	/*
-	 * (non-Javadoc)
+	 * This method checks if all the items in the list are drinks.
 	 * 
 	 * @see com.customer.bill.service.MenuItemService#hasAllDrinks()
 	 */
+	@Override
 	public boolean hasAllDrinks(List<MenuItem> items) {
-		// TODO Auto-generated method stub
-		return false;
+		return items.stream().allMatch(item -> item.getItemType().equals(ItemType.DRINK));
 	}
 
 	/*
-	 * (non-Javadoc)
+	 * This method returns true if the items has atleast one food item in it.
+	 * This can have hotfood or drinks along with food item.
 	 * 
 	 * @see com.customer.bill.service.MenuItemService#hasAtleastOneFoodItem()
 	 */
+	@Override
 	public boolean hasAtleastOneFoodItem(List<MenuItem> items) {
-		// TODO Auto-generated method stub
-		return false;
+		return items.stream().anyMatch(item -> item.getItemType().equals(ItemType.FOOD));
 	}
 
 	/*
-	 * (non-Javadoc)
+	 * This method returns true if the items has atleast one hot food item in
+	 * it.
 	 * 
 	 * @see com.customer.bill.service.MenuItemService#hasAtleastOneHotFoodItem()
 	 */
+	@Override
 	public boolean hasAtleastOneHotFoodItem(List<MenuItem> items) {
-		// TODO Auto-generated method stub
-		return false;
+		return items.stream().anyMatch(item -> item.getItemType().equals(ItemType.HOTFOOD));
 	}
-
 }

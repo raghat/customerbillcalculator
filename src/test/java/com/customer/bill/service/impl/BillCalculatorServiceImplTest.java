f6985c85eccd1338f8d79644507305942901ca76
Raghavender Tangallapally <raghu.t29@gmail.com>
2017-06-19 13:29:49 +0100

    Completed test

--- src/test/java/com/customer/bill/service/impl/BillCalculatorServiceImplTest.java
+++ src/test/java/com/customer/bill/service/impl/BillCalculatorServiceImplTest.java
@@ -3,7 +3,13 @@
  */
 package com.customer.bill.service.impl;
 
+import static org.junit.Assert.assertTrue;
+import static org.mockito.Mockito.when;
+
+import java.math.BigDecimal;
+import java.util.Arrays;
 import java.util.Collections;
+import java.util.List;
 
 import org.junit.After;
 import org.junit.Before;
@@ -12,6 +18,8 @@ import org.junit.runner.RunWith;
 import org.mockito.Mock;
 import org.mockito.runners.MockitoJUnitRunner;
 
+import com.customer.bill.model.ItemType;
+import com.customer.bill.model.MenuItem;
 import com.customer.bill.service.BillCalculatorService;
 import com.customer.bill.service.ResourceBundleService;
 import com.customer.bill.service.ServiceChargeService;
@@ -28,10 +36,36 @@ public class BillCalculatorServiceImplTest {
 	@Mock
 	private ResourceBundleService resourceBundleService;
 	private BillCalculatorService billService;
+	List<MenuItem> items = Arrays.asList(new MenuItem("Cola", ItemType.DRINK),
+			new MenuItem("SteakSandwich", ItemType.HOTFOOD));
+	private BigDecimal totalPrice = new BigDecimal("5.0");
 
 	@Before
 	public void setUp() {
 		billService = new BillCalculatorServiceImpl(serviceChargeService, resourceBundleService);
+		when(serviceChargeService.getServiceCharge(items, totalPrice)).thenReturn(new BigDecimal("1.0"));
+		when(resourceBundleService.getConfigValue("ColaPrice")).thenReturn("0.5");
+		when(resourceBundleService.getConfigValue("SteakSandwichPrice")).thenReturn("4.5");
+	}
+
+	@Test
+	public void test_getTotalBillIncludingServiceCharge() {
+		// ARRANGE
+		BigDecimal expected = new BigDecimal("6.0");
+		// ACT
+		BigDecimal actual = billService.getTotalBillIncludingServiceCharge(items);
+		// ASSERT
+		assertTrue(expected.equals(actual));
+	}
+
+	@Test
+	public void test_getTotalBillExcludingServiceCharge() {
+		// ARRANGE
+		BigDecimal expected = new BigDecimal("5.0");
+		// ACT
+		BigDecimal actual = billService.getTotalBillExcludingServiceCharge(items);
+		// ASSERT
+		assertTrue(expected.equals(actual));
 	}
 
 	@Test(expected = NullPointerException.class)
@@ -62,5 +96,4 @@ public class BillCalculatorServiceImplTest {
 	public void tearDown() {
 		billService = null;
 	}
-
 }
